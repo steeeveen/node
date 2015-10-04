52f4cb5a061716e391be559da522ff7afa5bec6e
Your Name <you@example.com>
2015-10-05 10:32:31 +1100

    Made startup ping dns to check is database is up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 set -x
 
-#UPDATES MY OWN DNS SERVER
-echo $IS_KUBE
-if [ "$IS_KUBE" ]; then
-    echo "Updating DNS record"        
-    ipaddr=$(hostname -i)
-    curl -X PATCH -d '{"'$INSTANCE'":"'$ipaddr'"}' \
-          $DNS_ADDR'/dns.json'
-fi
-
-dnsrecords=$(curl $DNS_ADDR'/dns.json')
-dnsrecords="${dnsrecords:1:${#dnsrecords}-2}"
-echo $dnsrecords
-
-#EXPORTS DNS SERVER ADDRESSES AS IP
-IFS=',' read -a array <<< "$dnsrecords"
-for element in "${array[@]}"
+while [ "$redisActive" != "true" ]
 do
-    element=${element/:/=}
-    setenv="export $element"
-    eval $setenv
-done
+    dnsrecords=$(curl $DNS_ADDR'/dns.json')
+    dnsrecords="${dnsrecords:1:${#dnsrecords}-2}"
+    echo $dnsrecords
 
-#WAIT FOR THE REDIS SERVER TO BOOT
-#sleep 30
+    #EXPORTS DNS SERVER ADDRESSES AS IP
+    IFS=',' read -a array <<< "$dnsrecords"
+    for element in "${array[@]}"
+    do
+        element=${element/:/=}
+        setenv="export $element"
+        eval $setenv
+    done
+    sleep 5
+done
 
 exec "$@"
